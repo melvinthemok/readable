9c960cf645becb826e16db3af7352446f36f9767
melvinthemok <melvinmok.cy@gmail.com>
2017-10-07 16:13:48 +0800

    Add empty string check to passwordMatch

--- public/client_side_helpers/passwordMatch.js
+++ public/client_side_helpers/passwordMatch.js
@@ -3,9 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
   var password1 = document.getElementById('passwordDraft')
   var password2 = document.getElementById('password')
   var message = document.getElementById('passwordMessage')
-  var submitButton = document.getElementById('submitButton')
   password2.onkeyup = function () {
-    if (password1.value === password2.value) {
+    if (password1.value === password2.value && password1.value !== '' && password2.value !== '') {
       passwordFormGroup.classList.add('has-success')
       passwordFormGroup.classList.remove('has-warning')
       password1.classList.add('form-control-success')
