42783200c1d48929b6aea38e1fe16b4578a3385c
melvinthemok <melvinmok.cy@gmail.com>
2017-10-07 16:12:42 +0800

    Add show confirmation field to passwordLengthCheck

--- public/client_side_helpers/passwordLengthCheckAndShowConfirm.js
+++ public/client_side_helpers/passwordLengthCheckAndShowConfirm.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var passwordFormGroup = document.getElementById('password-form-group')
   var password1 = document.getElementById('passwordDraft')
+  // show password confirmation only onkeyup in passwordDraft
+  var description = document.getElementById('passwordDescription')
+  var password2 = document.getElementById('password')
+  // show alert if passwordDraft too short or too long
+  var passwordFormGroup = document.getElementById('password-form-group')
   var message = document.getElementById('passwordMessage')
-  var submitButton = document.getElementById('submitButton')
   password1.onkeyup = function () {
+    if (description.hasAttribute('style')) {
+      description.removeAttribute('style')
+    }
+    if (password2.hasAttribute('style')) {
+      password2.removeAttribute('style')
+    }
     if (password1.value.length > 7 && password1.value.length < 31) {
       passwordFormGroup.classList.remove('has-warning')
       password1.classList.remove('form-control-warning')
@@ -14,14 +23,12 @@ document.addEventListener('DOMContentLoaded', function () {
         passwordFormGroup.classList.add('has-warning')
       }
       message.textContent = 'Too short'
-      submitButton.setAttribute('disabled', true)
     } else {
       if (!password1.classList.contains('form-control-warning') && !passwordFormGroup.classList.contains('has-warning')) {
         password1.classList.add('form-control-warning')
         passwordFormGroup.classList.add('has-warning')
       }
       message.textContent = 'Too long'
-      submitButton.setAttribute('disabled', true)
     }
   }
 })
