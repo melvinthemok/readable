ab292d396ce506789a1ce34d2c0559b2018535d4
melvinthemok <melvinmok.cy@gmail.com>
2017-10-26 07:13:26 +0800

    Fix expLengthCheck bug

--- public/client_side_helpers/expLengthCheck.js
+++ public/client_side_helpers/expLengthCheck.js
@@ -4,21 +4,21 @@ document.addEventListener('DOMContentLoaded', function () {
   var message = document.getElementById('experienceMessage')
   experience.onkeyup = function() {
     if (experience.value.length < 501) {
-      if (experience.classList.contains('form-control-warning') && experienceFormGroup.classList.contains('form-control-warning')) {
+      if (experience.classList.contains('form-control-warning') && experienceFormGroup.classList.contains('has-warning')) {
         experience.classList.remove('form-control-warning')
         experienceFormGroup.classList.remove('has-warning')
       }
-      if (!experience.classList.contains('form-control-success') && !experienceFormGroup.classList.contains('form-control-success')) {
+      if (!experience.classList.contains('form-control-success') && !experienceFormGroup.classList.contains('has-success')) {
         experience.classList.add('form-control-success')
         experienceFormGroup.classList.add('has-success')
       }
       message.textContent = 'Characters remaining: ' + (500 - experience.value.length)
     } else {
-      if (experience.classList.contains('form-control-success') && experienceFormGroup.classList.contains('form-control-success')) {
+      if (experience.classList.contains('form-control-success') && experienceFormGroup.classList.contains('has-success')) {
         experience.classList.remove('form-control-success')
         experienceFormGroup.classList.remove('has-success')
       }
-      if (!experience.classList.contains('form-control-warning') && !experienceFormGroup.classList.contains('form-control-warning')) {
+      if (!experience.classList.contains('form-control-warning') && !experienceFormGroup.classList.contains('has-warning')) {
         experience.classList.add('form-control-warning')
         experienceFormGroup.classList.add('has-warning')
       }
