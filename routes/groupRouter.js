ae12db0394d2c9106f2a0fd0236f6f320b2182a1
melvinthemok <melvinmok.cy@gmail.com>
2018-05-03 22:10:26 +0800

    Remove obsolete files

--- routes/groupRouter.js
+++
@@ -1,7 +0,0 @@
-// var express = require('express')
-// var router = express.Router()
-// var groupController = require('../controllers/groupController')
-
-// router.get('/', groupController.getGroup)
-
-// module.exports = router
